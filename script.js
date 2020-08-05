2d1864de6c41e7b8185e3943bc3ae8b290ce6665
Kingsley <kingsleyumujeyan@gmail.com>
2020-08-05 13:36:52 +0100

    updated footer and navbar icon

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Adding copyright year that updates
+const copyrightYear = document.getElementById("copyright");
+const year = document.createTextNode(new Date().getFullYear());
+copyrightYear.appendChild(year);
